bc3802bfdf870a8f3d37bdaa9d65d004236ccb74
Tahir <Tahir@192.168.4.199>
2016-11-19 15:13:07 -0500

    Loop

--- src/main/java/net/mybluemix/servlet/PushServlet.java
+++ src/main/java/net/mybluemix/servlet/PushServlet.java
@@ -27,7 +27,9 @@ public class PushServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Subscribe request successful !!! ");
+		while(true)
+			response.getWriter().append("Alert : Fire close by !!! ");
 	}
 
 	/**
