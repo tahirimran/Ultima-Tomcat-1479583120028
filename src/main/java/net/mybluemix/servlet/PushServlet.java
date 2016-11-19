bb31fe5cd8842b52c46f5faf92c34d51871249c8
Tahir <Tahir@192.168.4.199>
2016-11-19 16:36:55 -0500

    DB

--- src/main/java/net/mybluemix/servlet/PushServlet.java
+++ src/main/java/net/mybluemix/servlet/PushServlet.java
@@ -1,6 +1,10 @@
 package net.mybluemix.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +18,12 @@ import javax.servlet.http.HttpServletResponse;
 public class PushServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private static int counter = 0 ; 
+    
+    private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static final String DB_CONNECTION = "jdbc:db2://awh-yp-small03.services.dal.bluemix.net:50001/BLUDB:sslConnection=true;";
+	private static final String DB_USER = "dash104211";
+	private static final String DB_PASSWORD = "0oRYURuUy3rG";
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -38,8 +48,43 @@ public class PushServlet extends HttpServlet {
 	   }else{
 		response.getWriter().append("Alter: There is a  fire.... ");
 	   }
+	   Connection  con = getDBConnection();
+	   response.getWriter().append("Connection Successfull.... ");
+	   
+
 	}
 
+	
+	private static Connection getDBConnection() {
+
+		Connection dbConnection = null;
+
+		try {
+
+			Class.forName(DB_DRIVER);
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		try {
+
+			dbConnection = DriverManager.getConnection(
+                             DB_CONNECTION, DB_USER,DB_PASSWORD);
+			return dbConnection;
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		return dbConnection;
+
+	}
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
